0e63440f503a5d3087e20099216eb25621f10052
Mati <maticzacza@gmail.com>
2023-12-06 21:15:27 +0100

    azure deops integrae

---
+++ script.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import os
+
+pat = os.getenv('PTA')
+
+organization = '01169549'  
+project = 'Lab05-MWO'            
+
+url = f'https://dev.azure.com/{organization}/{project}/_apis/wit/workitems/$Bug?api-version=6.0'
+
+headers = {
+    'Content-Type': 'application/json-patch+json',
+    'Authorization': f'Basic {pat}'
+}
+
+work_item = [
+    {
+        "op": "add",
+        "path": "/fields/System.Title",
+        "value": "Tytuł błędu"  
+    },
+    {
+        "op": "add",
+        "path": "/fields/Microsoft.VSTS.TCM.ReproSteps",
+        "value": "Opis kroków reprodukujących błąd"  
+    }
+]
+
+response = requests.post(url, headers=headers, data=json.dumps(work_item))
+
+if response.status_code == 200:
+    print("Work Item utworzony pomyślnie.")
+    print("ID Work Item:", response.json()['id'])
+else:
+    print("Błąd przy tworzeniu Work Item:", response.content)
